0c27d5783dd2a001a19ca40c62cda567ededed65
T-IMAFUKU <amulet39@gmail.com>
2025-07-31 14:48:04 +0900

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Welcome to ShopWriter</h1>
-      <p>This is your Next.js + Stripe starter template.</p>
-    </div>
-  );
-}
