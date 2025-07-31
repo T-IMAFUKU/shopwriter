e9c03056d676ba831715dda1dee0f39819585497
T-IMAFUKU <amulet39@gmail.com>
2025-07-31 14:00:07 +0900

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to ShopWriter</h1>
-      <p>This is a SaaS service for generating EC content.</p>
-    </div>
-  );
+  return <h1>Welcome to ShopWriter</h1>;
 }
